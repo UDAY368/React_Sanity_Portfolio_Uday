8910ad1e85dde4868756c14e86c56c90bca173d9
K UDAY KUMAR <72476990+UDAY368@users.noreply.github.com>
2022-02-26 09:41:56 +0530

    Build the Wrapper Component Using HOC

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import { motion } from 'framer-motion';
 import { urlFor, client } from '../../client';
+import { AppWrap } from '../../wrapper';
 
 import './About.scss';
 
@@ -48,4 +49,4 @@ const About = () => {
   );
 }
 
-export default About;
+export default AppWrap(About, 'about');
